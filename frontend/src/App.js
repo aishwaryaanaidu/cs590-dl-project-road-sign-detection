6a520aaa197d2df618f3ec816cebc264ab50eced
Aishwarya Naidu <aishwaryanaidu@aishwaryas-mbp.notrust.clients.wireless.pfw.edu>
2022-10-26 22:32:01 -0400

    Code clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import WebcamCapture from './WebcamCapture';
 import Web from './Web';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <WebcamCapture /> */}
       <Web />
     </div>
   );
