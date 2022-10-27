54573c1222b37dedd2fca5f8b5287336fc6a8199
Aishwarya Madupuri Suresh <38344621+aishwaryaanaidu@users.noreply.github.com>
2022-10-27 00:55:24 -0400

    Aishwarya/input output processing (#2)
    * Adding UI skeleton code
    
    * Added code associated with
    
    * Added code associated with webcam
    
    * Fixing issues with getting frames
    
    * Complete working code
    
    * Code clean up
    
    * Code clean up
    
    Co-authored-by: Aishwarya Naidu <aishwaryanaidu@aishwaryas-mbp.notrust.clients.wireless.pfw.edu>

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Web from './Web';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Web />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Web.js
@@ -0,0 +1,80 @@
+import React, { useRef, useState, useEffect, useCallback } from 'react';
+import Webcam from 'react-webcam';
+
+function Web() {
+
+  const [isShowVideo, setIsShowVideo] = useState(false);
+  const videoElement = useRef();
+  const [imageSrc, setImageSrc] = useState("");
+  const [output, setOutput] = useState("");
+  const [receivedResponse, setReceivedResponse] = useState("");
+
+  useEffect(() => {
+    console.log(isShowVideo)
+    if(isShowVideo) {
+        const interval = setInterval(() => {
+            // console.log("videoElement" + videoElement.current.getScreenshot())
+            setImageSrc(videoElement.current.getScreenshot())
+            getRoadSign(videoElement.current.getScreenshot())
+          }, 2000);
+          return () => clearInterval(interval);
+    }
+  }, [isShowVideo])
+
+const getRoadSign = (image) => {
+    fetch('https://4287-34-138-88-244.ngrok.io', { 
+      method: 'POST',
+      headers: {
+      },
+      body: JSON.stringify({ "input": image })
+    }).then(response =>  response.json())
+    .then(resData => { 
+        setOutput(resData.prediction)
+        setReceivedResponse(true)
+    }
+    ).catch(
+      error => console.log(error)
+    );
+  };
+
+  const videoConstraints = {
+    width: 640,
+    height: 480,
+    facingMode: { exact: "environment" }
+    // facingMode: "user"
+  }
+
+  const startCam = () => {
+    setIsShowVideo(true);
+  }
+
+  const stopCam = () => {
+    let stream = videoElement.current.stream;
+    const tracks = stream.getTracks();
+    tracks.forEach(track => {
+      console.log("track" + track)
+      track.stop()
+    });
+    setIsShowVideo(false);
+    setOutput("")
+  }
+
+  
+
+  return (
+    <div>
+      <div className="camView">
+        {isShowVideo &&
+          <Webcam audio={false} id="vid" screenshotFormat="image/jpeg" ref={videoElement} videoConstraints={videoConstraints} />
+        }
+      </div>
+      <button onClick={startCam}>Start Video</button>
+      <button onClick={stopCam}>Stop Video</button>
+      <div width="100px">
+        <p style={{ fontSize: '20pt' }}>{output}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Web;
